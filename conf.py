a09cf6ca5fa51cd6e363246c8839a6aae09c5a88
Jingze <u7166705@anu.edu.au>
2021-06-15 15:49:32 +0800

    第三次结果提交,参数配置如下,结果保存在image_results中:     PPT第一页速度参数mu,时段二,第三行     s_point1 = 20 * 0.2777778     s_point2 = 40 * 0.2777778     d_point1 = 10     d_point2 = 30     s_mu = 2     s_sigma = 0.25     d_sigma = 5

--- conf.py
+++ conf.py
@@ -4,6 +4,12 @@ s_point1 = 20 * 0.2777778
 s_point2 = 40 * 0.2777778
 d_point1 = 10
 d_point2 = 30
-s_mu = 1.5
+s_mu = 2
 s_sigma = 0.25
 d_sigma = 5
+
+import direct,main,one_agent
+
+main.run()
+one_agent.run()
+direct.run()
--- direct.py
+++ direct.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-if __name__ == '__main__':
+def run():
     env = Env()
     tools = Tools()
     draw = DRAW()
