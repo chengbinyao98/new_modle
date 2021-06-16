e0b8d918fd93c047aa8691f1338595b0715657fe
Jingze <u7166705@anu.edu.au>
2021-06-16 08:49:12 +0800

    第二页第三段第一行 s_point1 = 20 * 0.2777778 s_point2 = 40 * 0.2777778 d_point1 = 10 d_point2 = 30 s_mu = 2.5 s_sigma = 0.01 d_sigma = 10

--- conf.py
+++ conf.py
@@ -1,12 +1,12 @@
 # 何竞择
 
-s_point1 = 20 * 0.2777778
-s_point2 = 40 * 0.2777778
-d_point1 = 10
-d_point2 = 30
-s_mu = 2
-s_sigma = 0.25
-d_sigma = 5
+s_point1 = 40 * 0.2777778
+s_point2 = 60 * 0.2777778
+d_point1 = 30
+d_point2 = 60
+s_mu = 2.5
+s_sigma = 0.01
+d_sigma = 10
 
 if __name__ == '__main__':
     import agent1.main1, agent2.main2, direct, main, one_agent
@@ -16,4 +16,4 @@ if __name__ == '__main__':
     main.run()
     one_agent.run()
     direct.run()
-    print("-----------------------------------------")
+    print("----- ------------------------------------")
